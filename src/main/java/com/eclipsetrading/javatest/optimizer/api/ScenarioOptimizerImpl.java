f331a0a591afbf3ba7596a602d9238eb0d919806
dasabz <dasabhishek057@gmail.com>
2023-01-31 09:42:31 +0800

    Initial

--- src/main/java/com/eclipsetrading/javatest/optimizer/api/ScenarioOptimizerImpl.java
+++ src/main/java/com/eclipsetrading/javatest/optimizer/api/ScenarioOptimizerImpl.java
@@ -2,6 +2,12 @@ package com.eclipsetrading.javatest.optimizer.api;
 
 import java.util.*;
 
+/***
+ * ScenarioOptimizerImpl calculates the optimizedCost for each underlying and
+ * only if it is less than equal to original cost(optimized gets preference in equal case but can be changed if desired)
+ * then it puts the optimized scenarios for that underying in the final collection
+ * else it uses the original normal scenarios for that underlying which have a lower cost compared to the optimized ones.
+ */
 public class ScenarioOptimizerImpl implements ScenarioOptimizer {
     @Override
     public Collection<Scenario> optimize(Collection<Scenario> originalScenarios) {
